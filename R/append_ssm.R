039f8204ef0cc46d3d4cb8e51639af4154308a88
Ian Jonsen <ian.jonsen@mq.edu.au>
2019-10-11 10:17:19 -0300

    add append fn

--- R/append_ssm.R
+++ R/append_ssm.R
@@ -2,19 +2,19 @@
 ##'
 ##' @description annotate SMRU tables so each event has SSM-derived lon,lat,x,y,x.se,y.se
 ##'
-##' @param cids SMRU campaign id(s)
 ##' @param smru SMRU table file - output of \code{pull_smru_tables}
 ##' @param fit final \code{foieGras} fit object
 ##'
 ##' @examples
 ##'
-##' @importFrom dplyr filter select mutate group_by "%>%" summarise left_join
+##' @importFrom dplyr filter select mutate group_by "%>%" summarise left_join distinct
+##' @importFrom tidyr unnest
 ##' @importFrom lubridate mdy_hms
 ##' @importFrom foieGras grab
 ##'
 ##' @export
 
-annotate_smru_tables <- function(cids, smru, fit) {
+annotate_smru_tables <- function(smru, fit, drop.ids = NULL) {
 
   ## general approx fun
   approx.fn <- function(x, smru.table, date.var) {
@@ -39,8 +39,8 @@ annotate_smru_tables <- function(cids, smru, fit) {
   ## ctd table
   ctd <- smru$ctd %>%
     mutate(ref = as.character(ref),
-           end.date = lubridate::mdy_hms(end.date, tz = "UTC")) %>%
-    filter(!ref %in% c("ct150-440BAT-16", "ct144-184BAT2-14"))
+           end.date = mdy_hms(end.date, tz = "UTC")) %>%
+    filter(!ref %in% drop.ids)  #c("ct150-440BAT-16", "ct144-184BAT2-14")
 
   ctd <- p %>%
     group_by(ref) %>%
@@ -53,8 +53,8 @@ annotate_smru_tables <- function(cids, smru, fit) {
   ## dive table
   dive <- smru$dive %>%
     mutate(ref = as.character(ref),
-           de.date = lubridate::mdy_hms(de.date, tz = "UTC")) %>%
-    filter(!ref %in% c("ct150-440BAT-16", "ct144-184BAT2-14"))
+           de.date = mdy_hms(de.date, tz = "UTC")) %>%
+    filter(!ref %in% drop.ids)
 
   dive <- p %>%
     group_by(ref) %>%
@@ -66,8 +66,8 @@ annotate_smru_tables <- function(cids, smru, fit) {
   ## haulout table
   haulout <- smru$haulout %>%
     mutate(ref = as.character(ref),
-           s.date = lubridate::mdy_hms(s.date, tz = "UTC")) %>%
-    filter(!ref %in% c("ct150-440BAT-16", "ct144-184BAT2-14"))
+           s.date = mdy_hms(s.date, tz = "UTC")) %>%
+    filter(!ref %in% drop.ids)
 
   haulout <- p %>%
     group_by(ref) %>%
@@ -78,8 +78,8 @@ annotate_smru_tables <- function(cids, smru, fit) {
   ## summary table
   ssummary <- smru$summary %>%
     mutate(ref = as.character(ref),
-           e.date = lubridate::mdy_hms(e.date, tz = "UTC")) %>%
-    filter(!ref %in% c("ct150-440BAT-16", "ct144-184BAT2-14"))
+           e.date = mdy_hms(e.date, tz = "UTC")) %>%
+    filter(!ref %in% drop.ids)
 
   ssummary <- p %>%
     group_by(ref) %>%
@@ -89,13 +89,13 @@ annotate_smru_tables <- function(cids, smru, fit) {
 
   ## diag table
   diag <- smru$diag %>%
-    mutate(d.date = lubridate::mdy_hms(d.date, tz = "UTC")) %>%
+    mutate(d.date = mdy_hms(d.date, tz = "UTC")) %>%
     left_join(. , deploy_meta, by = c("ref" = "device_id")) %>%
     mutate(release_date = ifelse(is.na(release_date), d.date, release_date)) %>%
     mutate(release_date = as.POSIXct(release_date, origin = "1970-01-01", tz = "UTC")) %>%
     filter(d.date >= release_date) %>%
     select(-release_date) %>%
-    filter(!ref %in% c("ct150-440BAT-16", "ct144-184BAT2-14"))
+    filter(!ref %in% drop.ids)
 
   diag <- f %>%
     group_by(ref) %>%
@@ -103,4 +103,6 @@ annotate_smru_tables <- function(cids, smru, fit) {
     unnest() %>%
     left_join(diag, ., by = c("ref", c("d.date" = "date"))) %>%
     distinct()
+
+  list(diag = diag, ctd = ctd, dive = dive, haulout = haulout, summary = summary)
 }
