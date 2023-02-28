94ab1bd3f95e8d57750372969dcc6f74aa64e2f0
Ian Jonsen <ian.jonsen@mq.edu.au>
2023-02-28 09:39:50 -0400

    remove browser

--- R/grab_QC.R
+++ R/grab_QC.R
@@ -278,7 +278,6 @@ grab_QC <- function(x, what = "fitted", cut = FALSE, as_sf = FALSE, normalise = 
                  })
 
                out <- as_tibble(out)
-               browser()
                if(cut & what != "data") out <- out %>% filter(keep)
 
              } else if (what == "rerouted") {
