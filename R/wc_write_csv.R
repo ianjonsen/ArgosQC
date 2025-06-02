1111cf6d6d51a0e266580e5d4dccf5d841c93b2a
Ian Jonsen <jonsen@stochastic-qc.org>
2025-06-02 12:12:31 -0300

    combine wc_write_X fn files

--- R/wc_write_csv.R
+++ R/wc_write_csv.R
@@ -111,3 +111,156 @@ wc_write_csv <- function(wc_ssm,
   return(invisible())
 
 }
+
+
+
+##' @title WC generic datafile write to .csv
+##'
+##' @description Write WC generic datafile to .csv - format specific to ATN program
+##'
+##' @param smru_ssm SSM-appended WC tag datafile - output of \code{append_ssm}
+##' @param meta metadata
+##' @param program Determines structure of output metadata. Currently, only `atn`.
+##' @param path path to write .csv files
+##' @param dropIDs individual ids to be dropped
+##' @param suffix suffix to add to .csv files (_nrt, _dm, or _hist)
+##'
+##'
+##' @importFrom dplyr filter rename mutate select any_of bind_rows group_by
+##' @importFrom dplyr group_split
+##' @importFrom stringr str_extract regex
+##' @importFrom lubridate mdy_hms
+##' @importFrom readr write_csv
+##' @importFrom purrr walk
+##' @importFrom snakecase to_snake_case
+##'
+##' @keywords internal
+
+wc_write_datafile <- function(wc_ssm,
+                              meta,
+                              program = "atn",
+                              path = NULL,
+                              dropIDs = NULL,
+                              suffix = "_nrt") {
+  stopifnot("A destination directory for .csv files must be provided" = !is.null(path))
+
+  ## file
+  tmp <- wc_ssm |>
+    filter(!DeploymentID %in% dropIDs) |>
+    filter(!is.na(DeploymentID))
+
+  out <- inner_join(tmp,
+                    meta |> select(DeploymentID, TagID, TagModel, AnimalAphiaID, ADRProjectID),
+                    by = "DeploymentID")
+
+  return(out)
+}
+
+
+
+##' @title ssmoutputs table write to .csv
+##'
+##' @description write to .csv - format depends on program (IMOS, ATN)
+##'
+##' @param smru_ssm SSM-appended WC tag datafile - output of \code{append_ssm}
+##' @param meta metadata
+##' @param program Determines structure of output metadata. Currently, either `imos` or `atn`.
+##' @param path path to write .csv files
+##' @param dropIDs individual ids to be dropped
+##' @param suffix suffix to add to .csv files (_nrt, _dm, or _hist)
+##'
+##'
+##' @importFrom dplyr filter rename mutate select left_join
+##' @importFrom readr write_csv
+##' @importFrom purrr walk
+##'
+##' @keywords internal
+
+wc_write_ssm <- function(locs_out,
+                         meta,
+                         program = "atn",
+                         path = NULL,
+                         dropIDs = NULL,
+                         suffix = "_nrt") {
+
+
+  locs_out <- locs_out |>
+    mutate(
+      lon = round(lon, 6),
+      lat = round(lat, 6),
+      x = round(x, 6),
+      y = round(y, 6),
+      x_se = round(x_se, 6),
+      y_se = round(y_se, 6),
+      u = round(u, 6),
+      v = round(v, 6),
+      u_se = round(u_se, 6),
+      v_se = round(v_se, 6),
+      s = round(s, 6),
+      s_se = round(s_se, 6)
+    )
+
+  if (suffix != "_nrt" & "keep" %in% names(locs_out)) {
+    ## cut predicted locs from large data gaps
+    locs_out <- locs_out |>
+      filter(keep) |>
+      select(-keep)
+
+  }
+
+  locs_out <- left_join(
+    locs_out,
+    meta |> select(DeploymentID, AnimalAphiaID, ADRProjectID),
+    by = "DeploymentID")
+
+
+  return(locs_out)
+}
+
+
+
+##' @title WC Locations datafile write to .csv
+##'
+##' @description Write WC Locations datafile to .csv - format specific to ATN program
+##'
+##' @param smru_ssm SSM-appended WC tag datafile - output of \code{append_ssm}
+##' @param meta metadata
+##' @param program Determines structure of output metadata. Currently, only `atn`.
+##' @param path path to write .csv files
+##' @param dropIDs individual ids to be dropped
+##' @param suffix suffix to add to .csv files (_nrt, _dm, or _hist)
+##'
+##'
+##' @importFrom dplyr filter rename mutate select any_of bind_rows group_by
+##' @importFrom dplyr group_split
+##' @importFrom stringr str_extract regex
+##' @importFrom lubridate mdy_hms
+##' @importFrom readr write_csv
+##' @importFrom purrr walk
+##' @importFrom snakecase to_snake_case
+##'
+##' @keywords internal
+
+wc_write_locations <- function(wc_ssm,
+                               meta,
+                               program = "atn",
+                               path = NULL,
+                               dropIDs = NULL,
+                               suffix = "_nrt") {
+  stopifnot("A destination directory for .csv files must be provided" = !is.null(path))
+
+
+  ## Argos data
+  locs <- wc_ssm$Locations |>
+    filter(!DeploymentID %in% dropIDs) |>
+    filter(!is.na(DeploymentID))
+
+  locs <- left_join(locs,
+                    meta |> select(DeploymentID, AnimalAphiaID, ADRProjectID),
+                    by = "DeploymentID")
+
+  return(locs)
+}
+
+
+
