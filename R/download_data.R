cf635847280e74dc7746e7f99629c4d194cdd634
Ian Jonsen <jonsen@stochastic-qc.org>
2025-08-29 11:16:24 -0300

    simplify to smru or wc data only

--- R/download_data.R
+++ R/download_data.R
@@ -1,18 +1,12 @@
-##' @title Download SMRU or Wildlife Computers tag data from arbitrary sources
+##' @title Download SMRU or Wildlife Computers tag data from their servers
 ##'
-##' @description Satellite tracking data, in a zipfile, are downloaded from a
-##' user-specified URL (for accessing zipfiles on GoogleDrive or Dropbox),
-##' accessed from the SMRU data server, or accessed from the Wildlife Computers
-##' Portal API via the `source` argument.
+##' @description Satellite tracking data are accessed from the SMRU data server,
+##' or accessed from the Wildlife Computers Portal API via the `source` argument.
 ##'
-##' @param path a url or local filepath pointing to a zipfile of tag datafiles
 ##' @param dest destination path to save download
 ##' @param source source type of data to be downloaded. Can be one of:
 ##' * `smru` - SMRU Data Server;
 ##' * `wc` - Wildlife Computers Data Portal API;
-##' * `googledrive` - a url link to a zipfile shared on a GoogleDrive;
-##' * `dropbox` - a url link to a zipfile shared on Dropbox;
-##' * `local` - a local filepath to a zipfile containing SMRU or WC tag datafiles.
 ##' @param unzip (logical) should the downloaded zipfile be unzipped.
 ##' @param cid SMRU tag deployment campaign id(s) to download, eg. "ct180"
 ##' @param user SMRU data server username as a quoted string
@@ -32,10 +26,8 @@
 ##'
 ##' @export
 
-download_data <- function(path = NULL,
-                     dest = NULL,
+download_data <- function(dest = NULL,
                      source = "smru",
-                     unzip = FALSE,
                      cid = NULL,
                      user = NULL,
                      pwd = NULL,
@@ -46,8 +38,7 @@ download_data <- function(path = NULL,
                      download = TRUE,
                      ...) {
 
-  source <- match.arg(source, choices = c("smru", "wc", "googledrive", "dropbox", "local"))
-  if(all(source == "local", (is.null(path) | is.null(dest)))) stop("A filepath containing a zipfile name must be provided")
+  source <- match.arg(source, choices = c("smru", "wc"))
 
 
   if(is.null(dest)) {
@@ -63,15 +54,7 @@ download_data <- function(path = NULL,
     }
   }
 
-  if(!source %in% c("smru", "wc", "local")) {
-    url <- create_download_url(path)
-
-    tidy_download(url, destdir = dest)
-
-    if(unzip)
-      unzip(dest, exdir = dest)
-
-  } else if(source == "smru") {
+  if(source == "smru") {
 
     smru_get_mdb(dest = dest, cid = cid, user = user, pwd = pwd, ...)
 
@@ -88,38 +71,7 @@ download_data <- function(path = NULL,
                  download = download,
                  return.tag.meta = TRUE)
 
-  } else if(source == "local") {
-
-    unzip(path, exdir = dest, setTimes = TRUE)
   }
 
   if(exists("meta")) return(meta)
-}
-
-
-##' @title download helper fn, borrowed from usethis:::tidy_download - without the interactivity
-##'
-##' @description helper fn
-##'
-##' @param url url for data
-##' @param destdir destination path to save download
-##'
-##' @importFrom fs file_temp file_move path path_file
-##'
-##' @keywords internal
-tidy_download <- function (url, destdir = getwd())
-{
-  options(usethis.quiet = TRUE)
-  usethis:::check_path_is_directory(destdir)
-  tmp <- file_temp("tidy-download-")
-  h <- usethis:::download_url(url, destfile = tmp)
-  #  cli::cat_line()
-  cd <- usethis:::content_disposition(h)
-  base_name <- usethis:::make_filename(cd, fallback = path_file(url))
-  full_path <- path(destdir, base_name)
-
-  attr(full_path, "content-type") <- usethis:::content_type(h)
-  attr(full_path, "content-disposition") <- cd
-  file_move(tmp, full_path)
-  invisible(full_path)
 }
