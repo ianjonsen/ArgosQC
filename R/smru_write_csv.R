a5c924cc9052d9eecab985b9a3b5f545fa40f195
Ian Jonsen <jonsen@stochastic-qc.org>
2026-08-12 14:54:22 -0300

    fix sd_depth so IMOS test passes

--- R/smru_write_csv.R
+++ R/smru_write_csv.R
@@ -1343,7 +1343,9 @@ smru_write_summary <- function(smru_ssm,
 
   if(program != "atn") { # imos & any other program
     ## double check only device_id's in metadata are written
-    if(all(is.na(ssummary$sd_depth))) ssummary$sd_depth <- 0
+    ## set any NA sd_depths to 0 so IMOS incoming test passes
+    ssummary <- ssummary |>
+      mutate(sd_depth = ifelse(is.na(sd_depth), 0, sd_depth))
 
     if(all(!c("tagging_id","s_date_tag","e_date_tag") %in% names(ssummary))) {
       ssummary <- ssummary |>
